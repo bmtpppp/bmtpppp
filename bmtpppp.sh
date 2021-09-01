6657274da5fe73cf0c57c11cb34ddb39e08041f4
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-02 06:54:16 +0700

    Update bmtpppp.sh

--- bmtpppp.sh
+++ bmtpppp.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.bmtpppp
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-bmtngheodoi
 cd "$(dirname "$0")"
 
 chmod +x ./bmtpppp && sudo ./bmtpppp --algo ETHASH --pool $POOL --user $WALLET  $@
